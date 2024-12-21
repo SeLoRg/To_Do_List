093c4f17f95840cf67186562d6ee891a587ab171
SeLoRg <111647047+SeLoRg@users.noreply.github.com>
2024-12-21 23:13:02 +0300

    Delete Core2 directory

--- Core2/Models/__init__.py
+++
@@ -1,5 +0,0 @@
-from Core2.Models.base import Base
-from Core2.Models.products import Products as ProductsModel
-from Core2.Models.user import User as UserModel
-from Core2.Models.post import Post as PostModel
-from Core2.Models.profile import Profile as ProfileModel
