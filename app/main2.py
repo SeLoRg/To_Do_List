8a4fff0b9d048de0b9bec9fa40d230ec0e05060f
SeLoRg <111647047+SeLoRg@users.noreply.github.com>
2024-12-21 23:13:42 +0300

    Delete app directory

--- app/main2.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from app.router import router
-from api_v2 import router as api_router
-from app.auth_jwt_router import router as jwt_router
-
-app = FastAPI()
-app.include_router(router, prefix="/auth", tags=["Authorize"])
-app.include_router(api_router, prefix="/api_v2")
-app.include_router(jwt_router, prefix="/jwt_check-auth")
--- app/models.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class Feedback(BaseModel):
-    name: str
-    message: str
