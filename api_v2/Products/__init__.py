2bab3e7749f360583c341bdca25c482ee1930d38
SeLoRg <111647047+SeLoRg@users.noreply.github.com>
2024-12-21 23:13:31 +0300

    Delete api_v2 directory

--- api_v2/Products/__init__.py
+++
@@ -1,4 +0,0 @@
-from .schemas import ProductsUpdatePartial as ProductsUpdatePartialSchema
-from .schemas import Products as ProductsSchema
-from .schemas import ProductsCreate as ProductsCreateSchema
-from .schemas import ProductsUpdate as ProductsUpdateSchema
